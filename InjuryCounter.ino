7c81590836383907560f0534c65778d6a95a4792
James Kemp-Dale <jameskempdale@gmail.com>
2025-01-16 20:56:34 +1300

    Created Seven Segment Display class, initialization function and barebones function to create 8 pin numerical configurations for display

--- InjuryCounter.ino
+++ InjuryCounter.ino
@@ -1,3 +1,81 @@
 // Injury Counter Display
 // James Kemp-Dale
 // 16th January 2025
+
+// Class declarations for SevenSegDisplay
+class SevenSegDisplay {
+  public:
+    void initializeDisplay(int newDisplayPins[], bool newDriverAttached);
+    void generateDirectNum(int newNum);
+    // void generateDriverNum(int newNum);
+    bool driverAttached;
+  private:
+    int driverDisplayPins[4];
+    int directDisplayPins[8];
+};
+
+void SevenSegDisplay::initializeDisplay(int newDisplayPins[], bool newDriverAttached) {
+  // Depending on if a 4511 is declared to be utilized, fill the list of pin numbers according to newDisplayPins and turn the pins into outputs
+  
+  if (newDriverAttached) {
+    for (int x = 0; x < 4; x++) {
+      driverDisplayPins[x] = newDisplayPins[x];
+      pinMode(driverDisplayPins[x], OUTPUT);
+    }
+  } else {
+    for (int x = 0; x < 8; x++) {
+      directDisplayPins[x] = newDisplayPins[x];
+      pinMode(directDisplayPins[x], OUTPUT);
+    }
+  }
+};
+
+void SevenSegDisplay::generateDirectNum(int newNum) {
+  // Checks that the number is a single digit, maps to a configured 
+  // digit configuration for all 8 directly connected pins and outputs that configuration. Not the most elegant solution.
+  // Works when a 4511 Driver is not present
+
+  // Lays out a default list of 8 pin positions, all set to off.
+  int pinMapping[] = {0,0,0,0,0,0,0,0};
+  
+  // As long as newNum is a single digit then the 
+  if (newNum < 10) {
+    // Checks through a list of possible configurations according to newNum
+    // All cases are currently empty until testing occurs
+    switch(newNum) {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+      case 6:
+        break;
+      case 7:
+        break;
+      case 8:
+        break;
+      case 9:
+        break;
+    }
+
+    // Once the configuration is set then turn on all applicable pins
+    for (int pin = 0; pin < 8; pin++) {
+      digitalWrite(this->directDisplayPins[pin], *pinMapping);
+    }
+  }
+}
+
+void setup() {
+
+}
+
+void loop() {
+
+}
